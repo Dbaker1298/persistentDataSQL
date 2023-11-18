291ab76b1f8c0e49248838a17963239fec41a89d
David Baker <dpbaker1298@gmail.com>
2023-11-18 16:01:17 -0500

    add testing for sqlite3 file

---
+++ pomo/pomodoro/sqlite3_test.go
@@ -0,0 +1,33 @@
+//go:build !inmemory
+// +build !inmemory
+
+package pomodoro_test
+
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+
+	"github.com/Dbaker1298/pomo/pomodoro"
+	"github.com/Dbaker1298/pomo/pomodoro/repository"
+)
+
+func getRepo(t *testing.T) (pomodoro.Repository, func()) {
+	t.Helper()
+
+	tf, err := ioutil.TempFile("", "pomo")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	tf.Close()
+
+	dbRepo, err := repository.NewSQLite3Repo(tf.Name())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	return dbRepo, func() {
+		os.Remove(tf.Name())
+	}
+}
